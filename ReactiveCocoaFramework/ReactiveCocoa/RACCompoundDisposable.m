0aa1731963bb910e061d6e95465d52415a19301d
Eric Patey <eric@patey.com>
2014-08-22 16:57:36 -0400

    Don't bother adding an already disposed disposable to a RACCompoundDisposable in -addDisposable.

--- ReactiveCocoaFramework/ReactiveCocoa/RACCompoundDisposable.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACCompoundDisposable.m
@@ -124,7 +124,7 @@ static CFMutableArrayRef RACCreateDisposablesArray(void) {
 
 - (void)addDisposable:(RACDisposable *)disposable {
 	NSCParameterAssert(disposable != self);
-	if (disposable == nil) return;
+	if (disposable == nil || disposable.isDisposed) return;
 
 	BOOL shouldDispose = NO;
 
